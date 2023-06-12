bef389d8caca882d38b9ef5e77f74affd4088e04
tdu <tommy.deudaeta@gmail.com>
2023-06-12 11:09:34 -0300

    hermoso el git status con el hash

--- git_status.sh
+++ git_status.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+target_directory=${1:-.}
+
+if [[ ! -d "$target_directory/.git" ]]; then
+    echo ""
+    exit 1
+fi
 
 branch=$(git rev-parse --abbrev-ref HEAD)
 status=$(git status --porcelain)
-
+commit_hash=$(git rev-parse --short HEAD)
 if [ -n "$status" ]; then
-  echo "$branch*"
+  echo "$branch* "
 else
-  echo "$branch"
+  echo "$branch "
 fi
 
+echo "$commit_hash"
